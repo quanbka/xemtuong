257db4336d3f296b59a3a2875138eea10e7ee606
Le Van Quan <quanbka.cntt@gmail.com>
2022-01-14 21:25:10 +0700

    fix

--- app/Jobs/Crawl.php
+++ app/Jobs/Crawl.php
@@ -7,7 +7,7 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use App\Tuvi;
+use App\TuVi;
 
 class Crawl implements ShouldQueue
 {
